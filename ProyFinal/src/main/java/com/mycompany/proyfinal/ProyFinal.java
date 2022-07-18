17ef0d089f88d680a2d7df75e74ae426877ce801
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 00:02:41 -0600

    Inicio de la progra

--- ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
+++ ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
@@ -1,17 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.proyfinal;
+import java.util.Scanner;
 
-/**
- *
- * @author josep
- */
 public class ProyFinal {
-
+    private Scanner teclado;
+    private String[] nombre;
+    private String[] telefono;
+    private String[] email;
+    public void cargar(){
+        teclado=new Scanner(System.in);
+        nombre=new String [5];
+        telefono =new String [5];
+        email =new String [5];
+        for (int i=0;i<5;i++){
+            System.out.println("Ingrese el nombre del cliente: ");
+            nombre[i]=teclado.next();
+        }
+        for (int i=0;i<5;i++){
+            System.out.println("Ingrese el telefono del cliente: ");
+            telefono[i]=teclado.next();
+        }
+        for (int i=0;i<5;i++){
+            System.out.println("Ingrese el email del cliente: ");
+            email[i]=teclado.next();
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ProyFinal c =new ProyFinal();
+        c.cargar();
     }
 }
