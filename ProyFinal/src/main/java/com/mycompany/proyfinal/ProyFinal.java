cd3e1e5564d580e09cfaca6bfebe53019a229e38
jmunozc023 <67330305+jmunozc023@users.noreply.github.com>
2022-07-20 03:47:24 -0600

    Update

--- ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
+++ ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
@@ -1,18 +1,26 @@
 package com.mycompany.proyfinal;
 
+import com.mycompany.model.Cliente;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
 
 public class ProyFinal {
     
    
     
-    
     public static void main(String[] args) {
-        var usuario=new Clientes();
-        usuario.nombre();
-        usuario.telefono();
-        usuario.email();
-        System.out.println(usuario.nombre);
-        System.out.println(usuario.telefono);
-        System.out.println(usuario.email);
+        var cliente1=new Cliente();
+        ArrayList<Cliente> arrayOfCliente=new ArrayList<>();
+        cliente1.getNombre();
+        cliente1.getTelefono();
+        cliente1.getEmail();
+        arrayOfCliente.add(cliente1);
+        for (int i=0; i<arrayOfCliente.size();i++){
+            System.out.println(arrayOfCliente.get(i));
+            
+        }
+        
     }
+    
 }
