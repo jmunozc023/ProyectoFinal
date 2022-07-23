8406bc047160f18fc5677b0c481f3efaf96267c4
jmunozc023 <67330305+jmunozc023@users.noreply.github.com>
2022-07-22 20:48:00 -0600

    Update
    Funciona en consola, mantenimiento de empleados

---
+++ ProyFinal/src/main/java/com/mycompany/proyfinal/Cliente.java
@@ -0,0 +1,63 @@
+package com.mycompany.proyfinal;
+
+public class Cliente {
+    int clnumero;
+    String clnombre;
+    int clnumeroTel;
+    String clemail;
+    int getClnumero;
+    String getClnombre;
+    int getClnumeroTel;
+    String getClemail;
+    public Cliente(int clnumero, String clnombre, int clnumeroTel, String clemail) {
+        this.clnumero = clnumero;
+        this.clnombre = clnombre;
+        this.clnumeroTel = clnumeroTel;
+        this.clemail = clemail;
+    }
+
+    Cliente() {
+        
+    }
+
+    public int getClnumero() {
+        return clnumero;
+    }
+
+    public void setClnumero(int clnumero) {
+        this.clnumero = clnumero;
+    }
+
+    public String getClnombre() {
+        return clnombre;
+    }
+
+    public void setClnombre(String clnombre) {
+        this.clnombre = clnombre;
+    }
+
+    public int getClnumeroTel() {
+        return clnumeroTel;
+    }
+
+    public void setClnumeroTel(int clnumeroTel) {
+        this.clnumeroTel = clnumeroTel;
+    }
+
+    public String getClemail() {
+        return clemail;
+    }
+
+    public void setClemail(String clemail) {
+        this.clemail = clemail;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente" + "Numero:" + clnumero + ", Nombre:" + clnombre + ", Telefono:" + clnumeroTel + ", Email:" + clemail;
+    }
+
+    
+    
+   
+}
--- ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
+++ ProyFinal/src/main/java/com/mycompany/proyfinal/ProyFinal.java
@@ -1,25 +1,65 @@
 package com.mycompany.proyfinal;
 
-import com.mycompany.model.Cliente;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class ProyFinal {
     
    
     
     public static void main(String[] args) {
-        var cliente1=new Cliente();
-        ArrayList<Cliente> arrayOfCliente=new ArrayList<>();
-        cliente1.getNombre();
-        cliente1.getTelefono();
-        cliente1.getEmail();
-        arrayOfCliente.add(cliente1);
-        for (int i=0; i<arrayOfCliente.size();i++){
-            System.out.println(arrayOfCliente.get(i));
-            
-        }
+                
+        int opcion=-1;
+        Scanner teclado= new Scanner(System.in);
+        Scanner teclado1= new Scanner(System.in);
+        List<Cliente> cl =new ArrayList<Cliente>();
+        do {            
+            System.out.println("1. Ingreso de clientes");
+            System.out.println("2. Mostrar clientes");
+            System.out.println("3. Borrar cliente");
+            System.out.println("4. Modificar clientes");
+            System.out.println("0. Salir");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el numero de Cliente: ");
+                    int clnum= teclado.nextInt();
+                    System.out.print("Ingrese el nombre del Cliente: ");
+                    String clnom= teclado1.nextLine();
+                    System.out.print("Ingrese el numero de Cliente: ");
+                    int clnumTel= teclado.nextInt();
+                    System.out.print("Ingrese el email del Cliente: ");
+                    String clem= teclado1.nextLine();
+                    cl.add(new Cliente(clnum,clnom,clnumTel,clem));
+                    
+                    break;
+                case 2:
+                    System.out.println("-------------------------------------------------");
+                    Iterator<Cliente> i = cl.iterator();
+                    while (i.hasNext()) {
+                        Cliente c = i.next();
+                        System.out.println(c.toString());
+                    }
+                    System.out.println("-------------------------------------------------");
+                    break;
+                case 3:
+                     
+                    break;
+                case 4:
+                    
+                    break;
+                case 0:
+                     
+                    break;     
+                 
+                default:
+                    throw new AssertionError();
+            }
+        } while (opcion !=0);
+
         
     }
     
